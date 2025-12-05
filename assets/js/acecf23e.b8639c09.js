f6b4a22d68c5e043ec7f74e62bbe987f7c9ec5d5
anthelion62 <anthelion62@users.noreply.github.com>
2025-12-05 18:17:22 +0000

    deploy: 1460d7e38d0faf00c64a17fa067d5a2ad9c5afeb

--- assets/js/acecf23e.b8639c09.js
+++
@@ -1,1 +0,0 @@
-"use strict";(globalThis.webpackChunkdocuments=globalThis.webpackChunkdocuments||[]).push([[1903],{1912:s=>{s.exports=JSON.parse('{"blogBasePath":"/blog","blogTitle":"Blog","authorsListPath":"/blog/authors"}')}}]);
---
+++ assets/js/acecf23e.fa976382.js
@@ -0,0 +1,1 @@
+"use strict";(globalThis.webpackChunkdocuments=globalThis.webpackChunkdocuments||[]).push([[1903],{1912:s=>{s.exports=JSON.parse('{"blogBasePath":"/documents/blog","blogTitle":"Blog","authorsListPath":"/documents/blog/authors"}')}}]);
